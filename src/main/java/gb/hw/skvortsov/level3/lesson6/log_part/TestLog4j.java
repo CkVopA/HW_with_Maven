8de26a77773309972821840258963400c19dcb42
Igor <bird-starling@ya.ru>
2022-05-11 22:27:59 +0300

    slf4j

--- src/main/java/gb/hw/skvortsov/level3/lesson6/log_part/TestLog4j.java
+++ src/main/java/gb/hw/skvortsov/level3/lesson6/log_part/TestLog4j.java
@@ -1,6 +1,25 @@
 package gb.hw.skvortsov.level3.lesson6.log_part;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TestLog4j {
+
+    public static final Logger logger = LoggerFactory.getLogger(TestLog4j.class);
+
+    public static void main(String[] args) {
+        foo();
+    }
+
+    public static void foo(){
+        logger.trace("!!!! trace!");
+        logger.debug("!!!! debug!");
+        logger.info(" !!!! info @!");
+        logger.warn("!!! warn !!");
+        logger.error("!!!! ERr!");
+    }
+
+
 
     // TRACE
     // DEBUG
